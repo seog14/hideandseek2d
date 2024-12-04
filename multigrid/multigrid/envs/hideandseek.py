5214685d0db464677d1c181010e218e33c97997a
seog14 <garrettseo@gmail.com>
2024-12-04 18:10:22 -0500

    Environment Grid generation

--- multigrid/multigrid/envs/hideandseek.py
+++ multigrid/multigrid/envs/hideandseek.py
@@ -1,8 +1,8 @@
 from __future__ import annotations
 import numpy as np
 import random
-import gen_env
 
+from multigrid.envs import gen_env
 from multigrid import MultiGridEnv
 from multigrid.core import Action, Grid, MissionSpace
 from multigrid.core.constants import *
@@ -23,9 +23,8 @@ class HideAndSeekEnv(MultiGridEnv):
         self,
         size: int = 15,
         max_steps: int = 100,
-        num_agents: int = 4,
-        agent_view_size: int = 1,
-        percent_trees: int = 25,
+        num_agents: int = 1,
+        percent_trees: int = .1,
         joint_reward: bool = False,
         **kwargs):
         
@@ -38,23 +37,20 @@ class HideAndSeekEnv(MultiGridEnv):
             agents = num_agents,
             width=size,
             height=size,
-            agent_view_size=agent_view_size,
             max_steps=max_steps,
-            agent_view_size=1,
             joint_reward=joint_reward,
             **kwargs,
         )
 
 
-    def _gen_grid(self):
+    def _gen_grid(self, width, height):
 
         # Create empty grid
-        self.step_count = 0
         self.grid = Grid(self.size, self.size)
+        print(self.grid_template)
         
-    
-        for i in len(self.grid_template): 
-            for j in len(self.grid_template[i]): 
+        for i in range(len(self.grid_template)): 
+            for j in range(len(self.grid_template[i])): 
                 if self.grid_template[i][j] == 1: 
                     self.grid.set(i, j, Wall())
                 elif self.grid_template[i][j] == 2: 
@@ -62,18 +58,17 @@ class HideAndSeekEnv(MultiGridEnv):
                 elif self.grid_template[i][j] == 3: 
                     self.hiding_spot = HidingSpot()
                     self.grid.set(i, j, self.hiding_spot)
-                elif self.grid.template[i][j] == 4: 
+                elif self.grid_template[i][j] == 4: 
                     self.key = Key()
                     self.grid.set(i, j, self.key)
         
+        # Place agents in the center
+        for agent in self.agents:
+            self.place_agent(agent, top=self.seeker, size=(1,1))
 
-
-
+        print("im done")
+    def step():
         pass 
-
-    def step() {
-        pass 
-    }
         
 
         
